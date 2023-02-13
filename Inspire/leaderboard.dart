478578e6beebb652a8642e341104c1f55467d6b2
AlcidesTheGurj <93118250+AlcidesTheGurj@users.noreply.github.com>
2023-02-13 18:08:55 +0200

    Add files via upload

--- Inspire/leaderboard.dart
+++ Inspire/leaderboard.dart
@@ -1,24 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:inspire/main.dart';
 
-class leaderBoard extends StatefulWidget {
-  final int score;
-  leaderBoard({Key? key, required this.score}) : super(key: key);
+void main() => runApp(const AppBarApp());
 
-  @override
-  State<leaderBoard> createState() => _leaderBoardState();
-}
+class AppBarApp extends StatelessWidget {
+  const AppBarApp({super.key});
 
-class _leaderBoardState extends State<leaderBoard> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        title: Text("Leaderboard"),
-      ),
-      body: Center(
-        child: Text("This is the Leaderboard the score is : $score "),
+    return MaterialApp(
+      home: const LeaderBoard(),
+    );
+  }
+}
 
+class LeaderBoard extends StatelessWidget {
+  const LeaderBoard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Leaderboard'),
+        backgroundColor: Colors.indigo,
+      ),
+      body: ListView.builder(
+        itemCount: 25,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: const Text(
+              "Team Name",
+              style: TextStyle(
+                fontSize: 25,
+                letterSpacing: 5,
+                color: Colors.white60,
+              ),
+            ),
+            leading: Text('${index + 1}',
+              style: const TextStyle(
+                fontSize: 30,
+                letterSpacing: 5,
+                color: Colors.yellowAccent,
+              ),),
+            trailing: const Text(
+              "pts",
+              style: TextStyle(
+                fontSize: 20,
+                letterSpacing: 1,
+                fontWeight: FontWeight.bold,
+                color: Colors.indigo,
+              ),
+            ),
+          );
+        },
       ),
     );
   }
