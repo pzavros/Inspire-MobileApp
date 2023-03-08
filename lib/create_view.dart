3158c67c2ca1e91763d3626fab83e31e6a53422d
VGuowei <basiliou.guowei@gmail.com>
2023-03-08 20:24:55 +0200

    add question functionality
    not good enough, need help

--- lib/create_view.dart
+++ lib/create_view.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CreateView extends StatefulWidget {
   const CreateView({Key? key}) : super(key: key);
@@ -11,6 +13,16 @@ class CreateView extends StatefulWidget {
 class _CreateViewState extends State<CreateView> {
   Query db = FirebaseDatabase.instance.ref().child('Competitions');
   final nameController = TextEditingController();
+  final qController = TextEditingController();
+  final aController = TextEditingController();
+  // String ques="";
+  // String answ="";
+   int time=0;
+
+  int cntQ=0;
+  List <Question> listOfQuestions=[];
+  //late Questions qObj;
+
 
   late DatabaseReference dbRef;
   // default pop up message
@@ -21,7 +33,12 @@ class _CreateViewState extends State<CreateView> {
   void initState() {
     super.initState();
     dbRef = FirebaseDatabase.instance.ref("Competitions");
+    // Question q=Question(question: "question", answer: "answer", timer: 1);
+    // String j=jsonEncode(q);
+    // debugPrint(j);
   }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -53,7 +70,7 @@ class _CreateViewState extends State<CreateView> {
                           border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(10.0),
                           ),
-                          labelText: 'Name ...',
+                          labelText: 'Name',
                         ),
                         validator: (String? value) {
                           return (value!.isEmpty) ? 'Name is Required!' : '';
@@ -67,53 +84,61 @@ class _CreateViewState extends State<CreateView> {
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              Text(
-                                'Question 1:',
+                               Text(
+                                'Question ${cntQ + 1}:',
                                 style: TextStyle(fontSize: 24),
                               ),
                               IconButton(
-                                onPressed: () {},
+                                // add question
+                                onPressed: () {
+
+                                },
                                 icon: const Icon(Icons.delete),
-                                // style: ButtonStyle(
-                                //     //backgroundColor:
-                                //     ),
-                              )
+
+                              ),
                             ],
                           ),
                           Container(
                             child: Column(
                               children: [
                                 TextFormField(
+                                  controller: qController,
                                   decoration: InputDecoration(
                                     border: OutlineInputBorder(
                                       borderRadius: BorderRadius.circular(10.0),
                                     ),
-                                    labelText: 'Question ...',
+                                    labelText: 'Question',
                                   ),
                                 ),
-                                SizedBox(
+                                const SizedBox(
                                   height: 10,
                                 ),
                                 TextFormField(
+                                  controller: aController,
                                   decoration: InputDecoration(
                                     border: OutlineInputBorder(
                                       borderRadius: BorderRadius.circular(10.0),
                                     ),
-                                    labelText: 'Answer ...',
+                                    labelText: 'Answer',
                                   ),
                                 ),
-                                SizedBox(
+                                const SizedBox(
                                   height: 10,
                                 ),
                                 TextFormField(
+                                  keyboardType: TextInputType.number,
+                                  inputFormatters: <TextInputFormatter>[
+                                    FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
+                                    FilteringTextInputFormatter.digitsOnly
+                                  ],
                                   decoration: InputDecoration(
                                     border: OutlineInputBorder(
                                       borderRadius: BorderRadius.circular(10.0),
                                     ),
-                                    labelText: 'Timespan ...',
+                                    labelText: 'Timespan (min)',
                                   ),
                                 ),
-                                SizedBox(
+                                const SizedBox(
                                   height: 10,
                                 ),
                               ],
@@ -143,17 +168,28 @@ class _CreateViewState extends State<CreateView> {
                     // dbRef.onValue.listen((event) {
                     //   debugPrint("debugPrint---------->${event.snapshot.value}");// prints the obj
                     // });
+                    // competition name
                     if (nameController.text.isNotEmpty) {
                       //TODO when name not empty, we also needs to check if name already exist in the database
                       //...
                       popupTitle = "Success";
                       popupMsg = "New data has been added to the database.";
-                      Map<String, String> competition = {
+                      Map<String, dynamic> competition = {
                         'name': nameController.text,
-
+                        'totalQ':cntQ,
+                        'questions':jsonEncode(listOfQuestions)
                       };
+                      // ideally it should look like this
+                      // Map<String, dynamic> competition = {
+                      //   'name': nameController.text,
+                      //   'totalQ':cntQ,
+                      //   'questions': {
+                      //     '1':{'question':"1+1", 'answer':"2", 'timer':1},
+                      //     '2':{'question':"1+1", 'answer':"2", 'timer':1},
+                      //     '3':{'question':"1+1", 'answer':"2", 'timer':1},
+                      //   }
+                      // };
                       dbRef.push().set(competition);
-                      //after push, a unique id is generated by the firebase, does not have a identifier
                     }
                     await showDialog(
                         context: context,
@@ -189,10 +225,53 @@ class _CreateViewState extends State<CreateView> {
             child: const Icon(Icons.add),
               onPressed: () {
                 //TODO add new question form (Q&A plus timespan)
+                setState(() {
+                  cntQ++;
+                  listOfQuestions.add(Question(question: qController.text,answer: aController.text,timer: time));
+                  //listOfQuestions.add(Questions(id: cntQ,question: Question(question: 'q',answer: 'a',timer: 1)));
+                  qController.clear();
+                  aController.clear();
+                });
+                debugPrint("debugPrint----------> ${cntQ}");
+                debugPrint("debugPrint----------> ${jsonEncode(listOfQuestions)}");
+                //listOfQuestions.asMap();
 
               },
           ),
           floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
         ));
   }
+}
+
+
+// class Questions {
+//   int id;
+//   Question question;
+//
+//   Questions({required this.question,required this.id });
+//
+//   void add(int id, Question q){
+//
+//   }
+//
+//
+// }
+
+
+class Question {
+  String question,answer;
+  int timer;
+
+  Question({required this.question,required this.answer, required this.timer});
+
+  Map toJson()=>{
+    'question':question,
+    'answer':answer,
+    'timer':timer,
+  };
+//testing
+// @override
+// String toString() {
+//   return '{question: $question, answer: $answer, time:$timer}';
+// }
 }
