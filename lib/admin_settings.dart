91c1e5477dc219e386ed014b2890bb3d2b227443
Panayiotis Zavros <panazavros8@gmail.com>
2023-04-06 23:51:33 +0300

    different buttons

--- lib/admin_settings.dart
+++ lib/admin_settings.dart
@@ -195,24 +195,23 @@ class _AdminPageState extends State<AdminPage> {
                   }
                 },
                 style: ElevatedButton.styleFrom(
-                  fixedSize: const Size(200, 50), // Set the button size
+                  fixedSize: const Size(150, 50), // Set the button size
                   shape: const RoundedRectangleBorder(
                     borderRadius: BorderRadius.all(
-                      Radius.circular(25), // Set the button shape
+                      Radius.circular(8), // Set the button shape
                     ),
                   ),
-                  primary: Colors.black, // Set the background color of the button
+                  primary: Colors.red, // Set the background color of the button
                   onPrimary: Colors.white, // Set the text color of the button
-                  padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 32), // Add some padding
+                  padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16), // Add some padding
                   textStyle: const TextStyle(
                     fontSize: 18, // Set the font size of the text
                     fontWeight: FontWeight.bold, // Set the font weight of the text
                     letterSpacing: 0.5, // Add some letter spacing for better readability
                   ),
-                  elevation: 8, // Set the elevation of the button
-                  shadowColor: Colors.grey.withOpacity(0.8), // Set the shadow color of the button
+                  elevation: 4, // Set the elevation of the button
+                  shadowColor: Colors.grey, // Set the shadow color of the button
                 ),
-
                 child: const Text(
                   'Close competition',
                   style: TextStyle(fontSize: 20),
